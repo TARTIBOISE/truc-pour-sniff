7a1edf4c48835dd3f9d04ef9c28d7d9a85af44e3
TARTIBOISE <rafaxel13@gmail.com>
2023-04-12 12:06:12 +0200

    mise de commentiares mieux

--- src/Validateur.php
+++ src/Validateur.php
@@ -1,16 +1,49 @@
 <?php
-/**ceci est un commentaire**/
-namespace app;
-
-class Validateur
-{/**un autre**/
-    public function check($input)
-    /**ça aussi c'est un commentaire**/
+ 
+/**
+ * Recipe class file
+ *
+ * PHP Version 5.2
+ *
+ * @category Recipe
+ * @package  Recipe
+ * @author   Lorna Jane Mitchell <lorna@ibuildings.com>
+ * @license  http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @link     http://example.com/recipes
+ */
+ 
+/**
+ * Recipe class
+ *
+ * The class holding the root Recipe class definition
+ *
+ * @category Recipe
+ * @package  Recipe
+ * @author   Lorna Jane Mitchell <lorna@ibuildings.com>
+ * @license  http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @link     http://example.com/recipes/recipe
+ */
+class Recipe
+{
+ 
+    protected $id;
+ 
+    public $name;
+ 
+    public $prep_time;
+ 
+    /**
+     * Get the ingredients
+     *
+     * This function calls a static fetching method against the Ingredient class
+     * and returns everything matching this recipe ID
+     *
+     * @return array An array of Ingredient objects
+     */
+    function getIngredients()
     {
-        if (strlen($input) < 2) {
-            return false;
-        } else {
-            return true;
-        }
+        $ingredients = Ingredient::fetchAllByRecipe($this->id);
+        return $ingredients;
     }
 }
+?>
