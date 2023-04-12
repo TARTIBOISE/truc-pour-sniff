3017a6b17dcb955ea569b17f96450d4f0ffc242b
TARTIBOISE <rafaxel13@gmail.com>
2023-04-12 11:57:04 +0200

    mise de commentiares

--- src/Validateur.php
+++ src/Validateur.php
@@ -1,10 +1,11 @@
 <?php
-
+//ceci est un commentaire
 namespace app;
 
 class Validateur
-{
+{//un autre
     public function check($input)
+    //ça aussi c'est un commentaire
     {
         if (strlen($input) < 2) {
             return false;
